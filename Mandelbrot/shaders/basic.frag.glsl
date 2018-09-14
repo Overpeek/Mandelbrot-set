0722bd9bce87b2d4313f789ae4764d307348298d
Overpeek <overpeek.fin@gmail.com>
2018-09-14 15:24:42 +0300

    multipower

--- Mandelbrot/shaders/basic.frag.glsl
+++ Mandelbrot/shaders/basic.frag.glsl
@@ -3,11 +3,18 @@
 in vec2 fragPos;
 
 uniform dvec2 unif_offset;
+uniform dvec2 unif_complex;
 uniform double unif_zoom;
 uniform int unif_iterations;
 
-#define COMPLEX_IMAGINARY -0.835
-#define COMPLEX_REAL -0.2321
+uniform sampler1D unif_tex;
+
+#define MANDELBROT false
+#define MULTIPOWER true
+#define POWER 10
+
+#define COMPLEX_REAL -0.8
+#define COMPLEX_IMAGINARY 0.156
 
 #define M_PI 3.1415926535897932384626433832795
 
@@ -19,17 +26,27 @@ int mandelbrot(double x, double y, int maximum) {
 	double xtemp;
 
     while (zx*zx + zy*zy <= 4.0 && iteration <= maximum) {
-		//JULIA SET
-		//xtemp = zx*zx - zy*zy;
-        //zy = 2*zx*zy + COMPLEX_IMAGINARY; 
-        //zx = xtemp - COMPLEX_REAL;
-		//JULIA SET
-			
-		//MANDELBROT SET
-		xtemp = zx*zx - zy*zy;
-        zy = 2*zx*zy + y; 
-        zx = xtemp + x;
-		//MANDELBROT SET
+		if (MANDELBROT) {
+			if (MULTIPOWER) {
+				xtemp = pow(float(zx)*float(zx) + float(zy)*float(zy), POWER / 2.0) * cos(POWER * atan(float(zy), float(zx))) + x;
+				zy = pow(float(zx)*float(zx) + float(zy)*float(zy), POWER / 2.0) * sin(POWER * atan(float(zy), float(zx))) + y;
+				zx=xtemp;
+			} else {
+				xtemp = zx*zx - zy*zy + x;
+				zy = 2*zx*zy + y; 
+				zx = xtemp;
+			}
+		} else {
+			if (MULTIPOWER) {
+				xtemp = pow(float(zx)*float(zx) + float(zy)*float(zy), POWER / 2.0) * cos(POWER * atan(float(zy), float(zx))) + unif_complex.x;
+				zy = pow(float(zx)*float(zx) + float(zy)*float(zy), POWER / 2.0) * sin(POWER * atan(float(zy), float(zx))) - unif_complex.y;
+				zx=xtemp;
+			} else {
+				xtemp = zx*zx - zy*zy + unif_complex.x;
+				zy = 2*zx*zy - unif_complex.y; 
+				zx = xtemp;
+			}
+		}
 		
         if (zx == y && zy == y) {
             return -1;
@@ -53,80 +70,11 @@ int mandelbrot(double x, double y, int maximum) {
     }
 }
 
-vec3 getColor1(int n, float maximum) {
+vec4 getColorLine(int n, float maximum) {
 	if (n < maximum && n > 0) {
-		int i = n % 16;
-		vec3 color;
-		switch (i){
-			case 0:
-			color = vec3(66, 30, 15);
-			break;
-
-			case 1:
-			color = vec3(25, 7, 26);
-			break;
-
-			case 2:
-			color = vec3(9, 1, 47);
-			break;
-
-			case 3:
-			color = vec3(4, 4, 73);
-			break;
-
-			case 4:
-			color = vec3(0, 7, 100);
-			break;
-			
-			case 5:
-			color = vec3(12, 44, 138);
-			break;
-
-			case 6:
-			color = vec3(24, 82, 177);
-			break;
-
-			case 7:
-			color = vec3(57, 125, 209);
-			break;
-
-			case 8:
-			color = vec3(134, 181, 229);
-			break;
-			
-			case 9:
-			color = vec3(211, 236, 248);
-			break;
-
-			case 10:
-			color = vec3(241, 233, 191);
-			break;
-
-			case 11:
-			color = vec3(248, 201, 95);
-			break;
-
-			case 12:
-			color = vec3(255, 170, 0);
-			break;
-
-			case 13:
-			color = vec3(204, 128, 0);
-			break;
-
-			case 14:
-			color = vec3(153, 87, 0);
-			break;
-
-			case 15:
-			color = vec3(106, 52, 3);
-			break;
-
-			default:
-			break;
-		}
-		return color / 255;
-	} else return vec3(0.0);
+		float i = sin(n / maximum);
+		return texture(unif_tex, i);
+	} else return vec4(0.0);
 }
 
 vec3 getColor2(int n, float maximum) {
@@ -164,5 +112,5 @@ void main() {
 	
 	//int value = julia(startX, startY, maximum);
 	int value = mandelbrot(startX, startY, maximum);
-	gl_FragColor = vec4(getColor2(value, maximum), 1.0);
+	gl_FragColor = getColorLine(value, maximum);
 }
--- Mandelbrot/tools/textureloader.h
+++ Mandelbrot/tools/textureloader.h
@@ -3,11 +3,12 @@
 #pragma once
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "stb_image.h"
+
 #include <GL/glew.h>
 #include <string>
 #include <fstream>
-
+#include <iostream>
 
 namespace tools {
 
@@ -16,21 +17,23 @@ namespace tools {
 		int width, height, nrChannels;
 		GLubyte *data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
 		if (!data) {
-			std::cout << "ERROR 0: FAILED LOADING TEXTURE!\n";
+			std::cout << "Failed to load texture" << std::endl;
 			return 0;
 		}
 
 		GLuint texture;
 		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
+		glBindTexture(GL_TEXTURE_1D, texture);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);		stbi_image_free(data);
+		glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, width, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_1D);
+
+		stbi_image_free(data);
 	}
 
 }
